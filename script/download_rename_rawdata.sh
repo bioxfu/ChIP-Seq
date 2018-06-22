cc6e71bf07cb4ca2cc10d33ad6f5d0a51cc64211
Fu Xing <bio.xfu@gmail.com>
2018-06-22 08:45:31 +0800

    update

--- script/download_rename_rawdata.sh
+++ script/download_rename_rawdata.sh
@@ -4,6 +4,6 @@ mkdir fastq
 host=xfu@10.41.25.100
 rsync -var $host:$DIR ./fastq/
 
-ls ./fastq/*/*/* |./script/rush -k 'mv {} fastq/s{/%}_{%@.+[_.](R\d).fa*s*t*q.gz}.fastq.gz' --dry-run
+ls ./fastq/*/*/*.gz |./script/rush -k 'mv {} fastq/{/%}_{%@.+[_.](R\d).fa*s*t*q.gz}.fastq.gz' --dry-run
 
-ls ./fastq/*/*/* |./script/rush -k 'mv {} fastq/s{/%}_{%@.+[_.](R\d).fa*s*t*q.gz}.fastq.gz' 
+ls ./fastq/*/*/*.gz |./script/rush -k 'mv {} fastq/{/%}_{%@.+[_.](R\d).fa*s*t*q.gz}.fastq.gz' 
