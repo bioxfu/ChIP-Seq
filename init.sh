9533f3a1ec0cabb59f7cba8b39bd06e5a849bc0c
Fu Xing <bio.xfu@gmail.com>
2018-09-12 16:36:08 +0800

    update init.sh to generate stat folder

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@ conda env export > doc/environment.yml
 module add macs/1.4.2
 
 if [ ! -d fastqc ]; then
-	mkdir fastqc clean bam MAnorm track peak_1e-5 peak_1e-2 table figure RData matrix
+	mkdir fastqc clean bam MAnorm track peak_1e-5 peak_1e-2 table figure RData matrix stat
 fi
