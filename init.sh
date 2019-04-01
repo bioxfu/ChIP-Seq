bcddc40b0c2d3c20d7c6bd5765d022bc102f6db9
Fu Xing <bio.xfu@gmail.com>
2019-04-01 14:42:49 +0800

    update

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 source activate gmatic
 #conda env export > doc/environment.yml
-module add macs/1.4.2
+#module add macs/1.4.2
 
 if [ ! -d fastqc ]; then
 	mkdir fastqc clean bam MAnorm track peak_1e-5 peak_1e-2 table figure RData matrix stat meme-chip
